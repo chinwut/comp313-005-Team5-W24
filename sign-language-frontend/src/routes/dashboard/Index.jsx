6a525c13d6f506fe9e512d9a1e57baabe76bdc2f
chinwut <chinnawut@outlook.com>
2024-03-06 18:14:28 -0500

    [feat/merge-poc] merge frontend poc into this repo

--- sign-language-frontend/src/routes/dashboard/Index.jsx
+++ sign-language-frontend/src/routes/dashboard/Index.jsx
@@ -1,21 +1,7 @@
-import { Button, Text } from '@chakra-ui/react';
-import { signOutUser } from '../../auth';
-import { CenteredMiddleBox } from '../../components';
-import { AppName } from '../../utils';
-import logo from './../../logo.svg';
-
+import HandsCapture from '../../components/HandsCapture';
 const Dashboard = () => {
     return (
-        <CenteredMiddleBox p={5}>
-            <img src={logo} className="App-logo" alt="logo" />
-            <Text>
-                {AppName}
-            </Text>
-            <br />
-            <Button variant="outline" colorScheme="cyan" onClick={signOutUser}>
-                Sign out
-            </Button>
-        </CenteredMiddleBox>
+        <HandsCapture /> 
     );
 };
 
