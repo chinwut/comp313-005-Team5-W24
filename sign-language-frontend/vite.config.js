173512be4953ecb3fed30dfd25a3b9744d97e120
v <>
2024-03-06 21:08:45 -0500

    Add Mediapipe Holistic and Vite Plugin Mediapipe

--- sign-language-frontend/vite.config.js
+++ sign-language-frontend/vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { mediapipe } from 'vite-plugin-mediapipe';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    mediapipe({
+      'hands.js': [
+        'VERSION', 
+        'HAND_CONNECTIONS', 
+        'Hands', 
+      ],
+    }),
+  ],
   esbuild: {
     loader: 'jsx', // Treat .js files as .jsx
   },
