c0a66ca7def8eda93cc88536dd4f7e2f88a3364f
chinwut <chinnawut@outlook.com>
2024-03-06 20:47:53 -0500

    [Tim] Create workaround solution of mediapipe"

--- sign-language-frontend/vite.config.js
+++ sign-language-frontend/vite.config.js
@@ -1,10 +1,65 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs';
+import path from 'path';
 
+function mediapipe_workaround() {
+  return {
+    name: 'mediapipe_workaround',
+    load(id) {
+      const MEDIAPIPE_EXPORT_NAMES = {
+        'pose.js': [
+          'POSE_LANDMARKS', 
+          'POSE_CONNECTIONS', 
+          'POSE_LANDMARKS_LEFT', 
+          'POSE_LANDMARKS_RIGHT', 
+          'POSE_LANDMARKS_NEUTRAL', 
+          'Pose',
+          'VERSION',
+        ],
+        'hands.js': [
+          'VERSION', 
+          'HAND_CONNECTIONS', 
+          'Hands', 
+        ],
+        'camera_utils.js': [
+          'Camera', 
+        ],
+        'drawing_utils.js': [
+          'drawConnectors',
+          'drawLandmarks',
+          'lerp',
+        ],
+        'control_utils.js': [
+          'drawConnectors',
+          'FPS',
+          'ControlPanel',
+          'StaticText',
+          'Toggle',
+          'SourcePicker',
+          
+          // 'InputImage', not working with this export. Is defined in index.d.ts 
+          // but is not defined in control_utils.js
+          'InputImage',
+          
+          'Slider',
+        ],
+      }
+
+      let fileName = path.basename(id);
+      if (!(fileName in MEDIAPIPE_EXPORT_NAMES)) return null
+      let code = fs.readFileSync(id, 'utf-8');
+      for (const name of MEDIAPIPE_EXPORT_NAMES[fileName]) {
+        code += `exports.${name} = ${name};`;
+      }
+      return {code};
+    },
+  };
+}
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),mediapipe_workaround()],
   esbuild: {
     loader: 'jsx', // Treat .js files as .jsx
-  },
+  }
 })
