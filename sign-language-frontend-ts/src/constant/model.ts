3e4333d5478ec8192d30138105b24134d32cf742
chinwut <chinnawut@outlook.com>
2024-04-14 19:38:08 -0400

    [Tim] Update model

--- sign-language-frontend-ts/src/constant/model.ts
+++ sign-language-frontend-ts/src/constant/model.ts
@@ -3,7 +3,7 @@ const prefix = 'http://localhost:3000';
 export { prefix };
 
 const CONFIGS = {
-  keypointClassifierLabels: ['A', 'B','C','D','E','F','G','H','I'],
+  keypointClassifierLabels: ['A', 'B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
 };
 
 export default CONFIGS;
